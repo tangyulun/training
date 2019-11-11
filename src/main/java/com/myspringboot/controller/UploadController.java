878bff5cc589175e2f69ed9ad0fd7708098204c5
tangyulun <17673256238@163.com>
2019-11-11 21:07:34 +0800

    1

--- src/main/java/com/myspringboot/controller/UploadController.java
+++ src/main/java/com/myspringboot/controller/UploadController.java
@@ -1,12 +1,10 @@
 package com.myspringboot.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -20,8 +18,8 @@ public class UploadController {
     //Save the uploaded file to this folder
     private static String UPLOADED_FOLDER = "E://temp//";
 
-    @Autowired
-    private RestTemplate restTemplate;
+//    @Autowired
+//    private RestTemplate restTemplate;
 
     @GetMapping("/file")
     public String index() {
